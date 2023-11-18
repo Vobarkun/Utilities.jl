904f47e5a417feafac6508f29872c163d95347ce
Steffen Schultze <schultze.steffen@gmail.com>
2023-11-18 10:54:18 +0100

    fix name, improve multilines

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -1,6 +1,13 @@
-module Utils
+module Utilities
 
 using Makie, IJulia
+
+
+import REPL
+REPL.REPLCompletions.latex_symbols["\\fig"] = "fig = Figure(); ax = Axis(fig[1,1])";
+REPL.REPLCompletions.latex_symbols["\\figls"] = "fig = Figure(); ls = LScene(fig[1,1]; fixcam..., show_axis = false)";
+REPL.REPLCompletions.latex_symbols["\\ls"] = "fig = Figure(); ls = LScene(fig[1,1]; fixcam..., show_axis = false)";
+REPL.REPLCompletions.latex_symbols["\\angstrom"] = "Å";
 
 function easein(x)
     x < 0 && return zero(x)
@@ -22,6 +29,18 @@ function numpath(path)
     end
 end
 
+function liftevery(innode, dt)
+    t = Observable(time_ns())
+    outnode = Observable(innode[])
+    on(innode) do val
+        if time_ns() - t[] > dt * 1e9
+            t[] = time_ns()
+            outnode[] = val
+        end
+    end
+    outnode
+end
+
 function focus()
     display("text/javascript", "window.focus()")
     IJulia.clear_output()
@@ -37,11 +56,22 @@ function window(figlike, f = true)
     f && focus()
 end
 
-struct IntervalTicks
-    step
+function theme()
+    Theme(
+        resolution = (1000,1000), 
+        Volume = (
+            algorithm = :iso, 
+            isovalue = 1.0, 
+            isorange = 0.9, 
+            colorrange = (0, 2)
+        )
+    )
 end
-MakieLayout.get_tickvalues(t::IntervalTicks, vmin, vmax) = ceil(Int, vmin / t.step) * t.step : t.step : floor(Int, vmax / t.step) * t.step
+set_theme!() = Makie.set_theme!(theme())
 
+
+struct IntervalTicks step end
+MakieLayout.get_tickvalues(t::IntervalTicks, vmin, vmax) = ceil(Int, vmin / t.step) * t.step : t.step : floor(Int, vmax / t.step) * t.step
 const xlog10 = (xscale = log10, xticks = LogTicks(IntervalTicks(1)), xminorticksvisible=true, xminorticks = IntervalsBetween(9))
 const ylog10 = (yscale = log10, yticks = LogTicks(IntervalTicks(1)), yminorticksvisible=true, yminorticks = IntervalsBetween(9))
 
@@ -49,17 +79,39 @@ xinc!(ax, xs...) = vlines!(ax, collect(xs), color = :transparent)
 yinc!(ax, ys...) = hlines!(ax, collect(ys), color = :transparent)
 include!(ax, ys) = scatter!(ax, xs, ys, color = :transparent)
 
-function liftevery(innode, dt)
-    t = Observable(time_ns())
-    outnode = Observable(innode[])
-    on(innode) do val
-        if time_ns() - t[] > dt * 1e9
-            t[] = time_ns()
-            outnode[] = val
-        end
-    end
-    outnode
+smoothstep(x) = ifelse(x < 0, zero(x), ifelse(x > 1, one(x), x * x * (3 - 2x)))
+smoothstep(x, x0, x1) = smoothstep((x - x0) / (x1 - x0))
+smoothstep(x, x0, x1, y0, y1) = y0 + (y1 - y0) * smoothstep(x, x0, x1)
+
+
+function calculate_rgba(rgb1, rgb2, rgba_bg)::RGBAf
+    rgb1 == rgb2 && return RGBAf(rgb1.r, rgb1.g, rgb1.b, 1)
+    c1 = Float64.((rgb1.r, rgb1.g, rgb1.b))
+    c2 = Float64.((rgb2.r, rgb2.g, rgb2.b))
+    alphas_fg = 1 .+ c1 .- c2
+    alpha_fg = clamp(sum(alphas_fg) / 3, 0, 1)
+    alpha_fg == 0 && return rgba_bg
+    rgb_fg = clamp.((c1 ./ alpha_fg), 0, 1)
+    rgb_bg = Float64.((rgba_bg.r, rgba_bg.g, rgba_bg.b))
+    alpha_final = alpha_fg + (1 - alpha_fg) * rgba_bg.alpha
+    rgb_final = @. 1 / alpha_final * (alpha_fg * rgb_fg + (1 - alpha_fg) * rgba_bg.alpha * rgb_bg)
+    return RGBAf(rgb_final..., alpha_final)
 end
+
+function alpha_colorbuffer(figure)
+    scene = figure.scene
+    bg = scene.backgroundcolor[]
+    scene.backgroundcolor[] = RGBAf(0, 0, 0, 1)
+    b1 = copy(colorbuffer(scene))
+    scene.backgroundcolor[] = RGBAf(1, 1, 1, 1)
+    b2 = colorbuffer(scene)
+    scene.backgroundcolor[] = bg
+    return map(b1, b2) do b1, b2
+        calculate_rgba(b1, b2, RGBAf(1,1,1,0))
+    end
+end
+
+
 
 @recipe(MultiLines) do scene
     Attributes(
@@ -76,35 +128,34 @@ function Makie.plot!(sc::MultiLines{<:Tuple{AbstractVector{<:Real}, AbstractVect
     yss = sc[2]
     cs = sc.color
 
-    ls = Observable(Point2f[])
+    points = Observable(Point2f[])
     colors = Observable(Float64[])
 
     function update_plot(xs, yss, cs)
-        colors[]
-
-        empty!(ls[])
+        empty!(points[])
         empty!(colors[])
 
-        cs = isnothing(cs) ? range(0, 1, length = length(yss)) : cs
+        ts = isa(cs, AbstractArray) ? cs : range(0, 1, length = length(yss))
 
-        for (t, ys) in zip(cs, yss)
-            append!(ls[], Point2f.(xs, ys))
-            push!(ls[], Point2f(NaN, NaN))
+        for (t, ys) in zip(ts, yss)
+            append!(points[], Point2f.(xs, ys))
+            push!(points[], Point2f(NaN, NaN))
             append!(colors[], fill(t, length(ys) + 1))
         end
 
         colors[] = colors[]
+        points[] = points[]
     end
 
     Makie.Observables.onany(update_plot, xs, yss, cs)
 
     update_plot(xs[], yss[], cs[])
 
-    # if !isnothing(sc.color[])
-    #     lines!(sc, ls, color = sc.color, colormap = sc.colormap, linewidth = sc.linewidth)
-    # else
-    lines!(sc, ls, color = colors, colormap = sc.colormap, linewidth = sc.linewidth)
-    # end
+    if isnothing(cs[]) || isa(cs[], AbstractArray)
+        lines!(sc, points, color = colors, colormap = sc.colormap, linewidth = sc.linewidth)
+    else
+        lines!(sc, points, color = sc.color, colormap = sc.colormap, linewidth = sc.linewidth)
+    end
 
     sc
 end
@@ -121,6 +172,9 @@ function Makie.convert_arguments(P::Type{<:MultiLines}, yss::AbstractMatrix{<:Re
     (1:size(yss, 1), collect(eachcol(yss)))
 end
 
+
+
+linkCameras!(g::GridPosition) = linkCameras!(contents(g))
 linkCameras!(scenes...) = linkCameras!(scenes)
 function linkCameras!(contents)
     scenes = map(contents) do s
@@ -132,7 +186,6 @@ function linkCameras!(contents)
     end
     linkCams!(scenes)
 end
-
 
 function linkCams!(scenes)
     for scene in scenes[1:end]
